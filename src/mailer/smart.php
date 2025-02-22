c001e9ff5995d0fa9c2be9e4dc37f6538810a8b6
Delersing <imoroz404@gmail.com>
2025-02-22 23:56:44 +0300

    edit smart.php file? changes email recipient

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'delersing.r@gmail.com';                 // Наш логин
-$mail->Password = '070706';                           // Наш пароль от ящика
+$mail->Password = '0707062006Delersing';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('delersing.r@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('wenona2249@shouxs.com');     // Add a recipient
+$mail->addAddress('imoroz404@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
